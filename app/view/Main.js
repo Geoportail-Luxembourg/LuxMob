332536cd31128e61b06ead0054f042dfc85e0dcb
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-01-14 15:43:42 +0100

    Main view, with toolbar

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,49 +1,32 @@
 Ext.define('LuxMob.view.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'main',
+    extend: 'Ext.Container',
+    xtype: 'mainview',
     requires: [
-        'Ext.TitleBar',
-        'Ext.Video'
     ],
     config: {
         tabBarPosition: 'bottom',
 
         items: [
             {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
+                xtype: "toolbar",
+                docked: "top",
+                items: [{
+                    xtype: "searchfield",
+                    flex: 4,
+                    action: "search"
+                }, {
+                    xtype: "spacer"
+                }, {
+                    xtype: "button",
+                    iconCls: "layers",
+                    action: "layers",
+                    iconMask: true
+                }, {
+                    xtype: "button",
+                    iconCls: "settings",
+                    action: "settings",
+                    iconMask: true
+                }]
             }
         ]
     }
