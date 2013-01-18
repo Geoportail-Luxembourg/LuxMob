c701d5010a50c477cfe238084dc589103da8ab2b
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-01-18 14:09:58 +0100

    Translation for message in savedmaps

--- app/view/layers/SavedMaps.js
+++ app/view/layers/SavedMaps.js
@@ -5,6 +5,6 @@ Ext.define('App.view.layers.SavedMaps', {
     id: "savedmaps",
     config: {
         cls: "card",
-        html: "<p class='action'>Cette fonctionnalité n'est pas disponible.<br />Vous devez télécharger l'application pour y avoir accès.</p>"
+        html: "<p class='action'>" + i18n.message('savedmaps.html') + "</p>"
     }
 });
