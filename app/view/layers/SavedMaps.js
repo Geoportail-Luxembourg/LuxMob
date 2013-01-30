83ab872e5c62401cd77bef7f7b2f39a606a9d247
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-01-30 16:32:46 +0100

    SavedMaps view, first round

--- app/view/layers/SavedMaps.js
+++ app/view/layers/SavedMaps.js
@@ -2,11 +2,16 @@ window.i18n = Ext.i18n.Bundle;
 Ext.define('App.view.layers.SavedMaps', {
     extend: 'Ext.Panel',
 
+    requires: [
+        //'App.store.SavedMaps'
+    ],
+
     id: "savedmaps",
     config: {
-        layout: 'fit',
+//        layout: 'fit',
         items: [{ xtype: "toolbar",
             docked: "top",
+            title: i18n.message('savedmaps.title'),
             items: [{
                 xtype: "button",
                 text: i18n.message('button.close'),
@@ -14,9 +19,10 @@ Ext.define('App.view.layers.SavedMaps', {
                 iconMask: true
             }]
         }, {
-            xtype: 'panel',
-            cls: 'card',
-            html: "Saved maps are allowed"
+            xtype: 'list',
+            itemTpl: '<div>{name}</div>',
+            store: null
+            //store: 'SavedMaps'
         }]
     }
 });
