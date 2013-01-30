67b3696b46b233d28e741bf49945d6ea098c9c55
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-01-30 14:25:08 +0100

    Adding missing class for QueryDetail view

---
+++ app/view/QueryDetail.js
@@ -0,0 +1,26 @@
+window.i18n = Ext.i18n.Bundle;
+Ext.define('App.view.QueryDetail', {
+    extend: "Ext.Panel",
+
+    xtype: "querydetailview",
+
+    id: "queryDetailView",
+
+    config: {
+        fullscreen: true,
+        padding: 10,
+        tpl: '{html}',
+        data: null,
+        items: [{
+            docked: 'top',
+            xtype: 'toolbar',
+            title: i18n.message('query.detail.title'),
+            items: [{
+                xtype: "button",
+                text: i18n.message('button.back'),
+                ui: 'back',
+                action: "back"
+            }]
+        }]
+    }
+});
