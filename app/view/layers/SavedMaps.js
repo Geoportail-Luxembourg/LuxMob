6c80fb57f136cabc806b5625588be2bfead84275
Antoine Abt <antoine@abt.im>
2013-02-01 14:22:35 +0100

    coding standards

--- app/view/layers/SavedMaps.js
+++ app/view/layers/SavedMaps.js
@@ -22,7 +22,7 @@ Ext.define('App.view.layers.SavedMaps', {
             xtype: 'list',
             id: 'savedmapsList',
             itemTpl: new Ext.XTemplate(
-                '<div class=savedmapsline><h4>{name}</h4>',
+                '<div><h4>{name}</h4>',
                 '{[this.getProgress(values)]}',
                 '</div>',
                 {
@@ -30,7 +30,9 @@ Ext.define('App.view.layers.SavedMaps', {
                         if (values.done == 100) {
                             return '';
                         }
-                        return '<div class="progress"><div class="progress_value">'+values.done+'%</div><div style="width:'+values.done+'%" class="progress_bar">&nbsp;</div></div>';
+                        return '<div class="progress"><div class="progress_value">' +
+                            values.done +'%</div><div style="width:' +
+                            values.done +'%" class="progress_bar">&nbsp;</div></div>';
                     }
                 }
             ),
