dab0d026e2fce40d10c951cc189a099c1a0da1f4
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-02-22 14:40:14 +0100

    Reloading automatically the application when updated

--- app.js
+++ app.js
@@ -63,15 +63,7 @@ Ext.application({
     },
 
     onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
+        window.location.reload();
     },
 
     prepareI18n: function() {
