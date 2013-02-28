51f96017d2c01e257fe80d8d3fabe0f23eaf8842
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-02-28 10:45:39 +0100

    Moving MyMaps code to dedicated controller

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -12,7 +12,6 @@ Ext.define('App.controller.Main', {
         'Ext.field.Email'
     ],
     config: {
-        myMapPreview: null,
         refs: {
             mainView: '#mainView',
             moreMenu: '#moreMenu',
@@ -99,8 +98,7 @@ Ext.define('App.controller.Main', {
             'mapsettings': 'showMapSettings',
             'settings': 'showSettings',
             'search': 'showSearch',
-            'login': 'showLogin',
-            'main/map/:id': 'showMyMap'
+            'login': 'showLogin'
         }
     },
 
@@ -274,91 +272,6 @@ Ext.define('App.controller.Main', {
                 this.getMyMapsButton().hide();
             },
             scope: this
-        });
-    },
-
-    showMyMap: function(id) {
-        this.showMain();
-
-        var preview = this.getMyMapPreview();
-        if (!preview) {
-            preview = this.getMainView().add({
-                xtype: 'container',
-                cls: 'results-preview',
-                height: 50,
-                padding: 5,
-                style: {
-                    message: i18n.message('querying'),
-                    backgroundColor: 'white'
-                },
-                masked: {
-                    xtype: 'loadmask',
-                    indicator: false
-                },
-                items: [{
-                    xtype: 'button',
-                    ui: 'plain',
-                    text: ' ',
-                    height: '2.2em',
-                    cls: 'x-textalign-left',
-                    iconCls: 'delete',
-                    iconMask: true,
-                    iconAlign: 'right',
-                    listeners: {
-                        element: 'element',
-                        tap: function() {
-                        }
-                    }
-                }]
-            });
-            this.setMyMapPreview(preview);
-        } else {
-            preview.mask();
-            preview.show();
-        }
-        Ext.Animator.run({
-            element: preview.element,
-            easing: 'easeInOut',
-            out: false,
-            autoClear: false,
-            preserveEndState: true,
-            from: {
-                height: 0
-            },
-            to: {
-                height: preview.getHeight()
-            }
-        });
-
-        var tpl = new Ext.Template(
-            '{title}',
-            '<br/><small>{nb_features} ',
-            OpenLayers.i18n('mobile.features'),
-            '</small>'
-        );
-
-        function loadFeatures(mymap) {
-            Ext.Ajax.request({
-                url: 'http://geoportail-luxembourg.demo-camptocamp.com/~pierre_mobile/mymaps/' + mymap.uuid + '/features',
-                success: function(response) {
-                    var format = new OpenLayers.Format.GeoJSON();
-                    var features = format.read(response.responseText);
-
-                    preview.getAt(0).setText(tpl.apply({
-                        title: mymap.title + ' ...',
-                        nb_features: features.length
-                    }));
-                    preview.unmask();
-                }
-            });
-        }
-
-        Ext.Ajax.request({
-            url: 'http://geoportail-luxembourg.demo-camptocamp.com/~pierre_mobile/mymaps/' + id,
-            success: function(response) {
-                var mymap = Ext.JSON.decode(response.responseText);
-                loadFeatures(mymap);
-            }
         });
     }
 });
--- app/controller/MyMaps.js
+++ app/controller/MyMaps.js
@@ -5,7 +5,9 @@ Ext.define('App.controller.MyMaps', {
     ],
 
     config: {
+        myMapPreview: null,
         refs: {
+            mainView: '#mainView',
             myMapsView: {
                 selector: '#myMapsView',
                 xtype: 'mymapsview',
@@ -24,7 +26,8 @@ Ext.define('App.controller.MyMaps', {
             }
         },
         routes: {
-            'mymaps': 'showMyMaps'
+            'mymaps': 'showMyMaps',
+            'main/map/:id': 'showMyMap'
         }
     },
 
@@ -34,5 +37,90 @@ Ext.define('App.controller.MyMaps', {
             this.getMyMapsView(),
             animation
         );
+    },
+
+    showMyMap: function(id) {
+        this.getApplication().getController('Main').showMain();
+
+        var preview = this.getMyMapPreview();
+        if (!preview) {
+            preview = this.getMainView().add({
+                xtype: 'container',
+                cls: 'results-preview',
+                height: 50,
+                padding: 5,
+                style: {
+                    message: i18n.message('querying'),
+                    backgroundColor: 'white'
+                },
+                masked: {
+                    xtype: 'loadmask',
+                    indicator: false
+                },
+                items: [{
+                    xtype: 'button',
+                    ui: 'plain',
+                    text: ' ',
+                    height: '2.2em',
+                    cls: 'x-textalign-left',
+                    iconCls: 'delete',
+                    iconMask: true,
+                    iconAlign: 'right',
+                    listeners: {
+                        element: 'element',
+                        tap: function() {
+                        }
+                    }
+                }]
+            });
+            this.setMyMapPreview(preview);
+        } else {
+            preview.mask();
+            preview.show();
+        }
+        Ext.Animator.run({
+            element: preview.element,
+            easing: 'easeInOut',
+            out: false,
+            autoClear: false,
+            preserveEndState: true,
+            from: {
+                height: 0
+            },
+            to: {
+                height: preview.getHeight()
+            }
+        });
+
+        var tpl = new Ext.Template(
+            '{title}',
+            '<br/><small>{nb_features} ',
+            OpenLayers.i18n('mobile.features'),
+            '</small>'
+        );
+
+        function loadFeatures(mymap) {
+            Ext.Ajax.request({
+                url: 'http://geoportail-luxembourg.demo-camptocamp.com/~pierre_mobile/mymaps/' + mymap.uuid + '/features',
+                success: function(response) {
+                    var format = new OpenLayers.Format.GeoJSON();
+                    var features = format.read(response.responseText);
+
+                    preview.getAt(0).setText(tpl.apply({
+                        title: mymap.title + ' ...',
+                        nb_features: features.length
+                    }));
+                    preview.unmask();
+                }
+            });
+        }
+
+        Ext.Ajax.request({
+            url: 'http://geoportail-luxembourg.demo-camptocamp.com/~pierre_mobile/mymaps/' + id,
+            success: function(response) {
+                var mymap = Ext.JSON.decode(response.responseText);
+                loadFeatures(mymap);
+            }
+        });
     }
 });
