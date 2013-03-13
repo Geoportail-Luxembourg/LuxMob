1330a5a677667efb512664754701bf6116b175ec
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-03-13 17:20:37 +0100

    Use the wsgi url for the Query store

--- app/store/Query.js
+++ app/store/Query.js
@@ -1,11 +1,14 @@
 Ext.define('App.store.Query', {
     extend: 'Ext.data.Store',
-    requires: 'App.model.Query',
+    requires: [
+        'App.model.Query',
+        'App.util.Config'
+    ],
     config: {
         model: 'App.model.Query',
         proxy: {
             type: 'jsonp',
-            url : "http://app.geoportail.lu/bodfeature/search",
+            url: App.util.Config.getWsgiUrl() + 'bodfeature/search',
             callbackKey: 'cb',
             reader: {
                 type: 'json',
