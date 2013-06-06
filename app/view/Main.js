99a9f6904924dbf86e7c20843e50021326aff643
jaykayone <jaykayone@gmail.com>
2013-06-06 10:49:12 +0200

    removed console logging

--- app/view/Main.js
+++ app/view/Main.js
@@ -183,7 +183,6 @@ Ext.define('App.view.Main', {
 	    else{
 	        _paq.push(["setSiteId", "13"]);
 	    }
-	    console.log(_paq);
         var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
         g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
         })();
