00cc3555a581f78eca5e33ee0dd9625cda56b77a
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-05-26 09:24:18 +0200

    Adding splash and icons

---
+++ cordova-app/platforms/android/bin/geoportaillu-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/bin/geoportaillu-debug-unaligned.apk : \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/bin/geoportaillu.ap_ \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/bin/classes.dex \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/CordovaLib/bin/classes.jar \
---
+++ cordova-app/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/gen/com/c2c/LuxMob/R.java \
+ : /home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/res/drawable-xhdpi/icon.png \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/res/drawable-hdpi/icon.png \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/res/drawable/icon.png \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/res/values/strings.xml \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/res/drawable-mdpi/icon.png \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/res/xml/config.xml \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/res/drawable-ldpi/icon.png \
+/home/pierre/public_html/luxembourg_mobileevo/cordova-app/platforms/android/bin/AndroidManifest.xml \
