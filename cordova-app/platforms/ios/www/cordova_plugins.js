126f5797345a47e4f7d6219ca7a1ab5026ac2545
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-10-01 15:39:38 +0200

    Adding geolocation plugin. Fixes #26

--- cordova-app/platforms/ios/www/cordova_plugins.js
+++ cordova-app/platforms/ios/www/cordova_plugins.js
@@ -57,6 +57,34 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.file/www/DirectoryEntry.js",
         "id": "org.apache.cordova.file.DirectoryEntry",
         "clobbers": [
@@ -193,6 +221,7 @@ module.exports.metadata =
     "org.apache.cordova.device": "0.2.9",
     "org.apache.cordova.file-transfer": "0.4.3",
     "org.apache.cordova.inappbrowser": "0.5.1",
+    "org.apache.cordova.geolocation": "0.3.10",
     "org.apache.cordova.file": "1.1.0"
 }
 // BOTTOM OF METADATA
